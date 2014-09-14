8cc279d669ec5614f96645d4c66e2b45d4dd6f42
charalampos papaloizou <papaloizouc@gmail.com>
2014-09-14 04:10:27 +0100

    some crazy code

--- h4good/theapp/views.py
+++ h4good/theapp/views.py
@@ -5,18 +5,31 @@ from django.shortcuts import render
 
 from theapp.util import get_data, get_data_by_country
 
+# because the data comes from csv files...
+bad_cache = {}
+
 
 def home(request):
     return render(request, "home_4.html", {})
 
 
 def get_data_view(request, fname, year):
-    data = get_data(fname, year)
-    data = json.dumps(data)
+    key = '{}_{}'.format(fname, year)
+    try:
+        data = bad_cache[key]
+    except KeyError:
+        data = get_data(fname, year)
+        data = json.dumps(data)
+        bad_cache[key] = data
     return HttpResponse(data, content_type="application/json")
 
 
 def get_date_by_country_view(requset, fname, country):
-    data = get_data_by_country(fname, country)
-    data = json.dumps(data)
+    key = 'country_{}_{}'.format(fname, country)
+    try:
+        data = bad_cache[key]
+    except KeyError:
+        data = get_data_by_country(fname, country)
+        data = json.dumps(data)
+        bad_cache[key] = data
     return HttpResponse(data, content_type="application/json")
